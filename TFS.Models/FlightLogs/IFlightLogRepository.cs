a2304ca6778bfdbbc7cef71fafb2b2edda134035
Joseph Daigle <joseph@cridion.com>
2009-10-21 17:50:10 -0400

    added the interface or a flight log repository

---
+++ TFS.Models/FlightLogs/IFlightLogRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Centro.DomainModel;
+
+namespace TFS.Models.FlightLogs
+{
+    public interface IFlightLogRepository : IRepository
+    {
+        IEnumerable<MissionLog> GetAllMissionLogs();
+
+        IQueryable<MissionLog> QueryMissionLogs();
+        IQueryable<Mission> QueryMissions();
+        IQueryable<SquadronLog> QuerySquadronLogs();
+
+        MissionLog SaveOrUpdate(MissionLog missionLog);
+        SquadronLog SaveOrUpdate(SquadronLog squadronLog);
+        Mission SaveOrUpdate(Mission mission);
+
+        MissionLog CreateNewMissionLog(string aircraftModel, string aircraftSerialNumber, string location);
+
+    }
+}
