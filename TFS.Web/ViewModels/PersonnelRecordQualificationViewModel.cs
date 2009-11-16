f30190a236b9c4c475bf90f193c80646f370c361
Joseph Daigle <joseph@cridion.com>
2009-11-15 19:33:48 -0500

    missing file

---
+++ TFS.Web/ViewModels/PersonnelRecordQualificationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Centro.DomainModel;
+
+namespace TFS.Web.ViewModels
+{
+    public class PersonnelRecordQualificationViewModel : BaseEntity
+    {
+    }
+}
