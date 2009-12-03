474d70beb6466d2b1ba8055be4db3976babfebf4
Joseph Daigle <joseph@cridion.com>
2009-12-03 17:06:19 -0500

    added some basic message domain models

---
+++ TFS.Models/Messages/Announcement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TFS.Models.Users;
+using System.ComponentModel.DataAnnotations;
+
+namespace TFS.Models.Messages
+{
+    public class Announcement : Message
+    {
+        [Required]
+        public virtual User CreateBy { get; set; }
+
+        [Required]
+        public virtual string Content { get; set; }
+
+        [Required]
+        public virtual bool Urgent { get; set; }
+    }
+}
---
+++ TFS.Models/Messages/Message.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using TFS.Models.Validation;
+using Iesi.Collections;
+using TFS.Models.Users;
+using Iesi.Collections.Generic;
+
+namespace TFS.Models.Messages
+{
+    public class Message : BaseDomainObject
+    {
+        public Message()
+        {
+            SeenBy = new HashedSet<User>();
+        }
+
+        public virtual int? Id { get; private set; }
+
+        [DomainEquality, Required, StringLength(100)]
+        public virtual string Title { get; set; }
+
+        [DomainEquality, Required, DateTimeKind(DateTimeKind.Utc)]
+        public virtual DateTime ActiveFromDate { get; set; }
+
+        [DomainEquality, Required, DateTimeKind(DateTimeKind.Utc)]
+        public virtual DateTime ActiveToDate { get; set; }
+
+        public virtual ISet<User> SeenBy { get; set; }
+
+        public virtual void MarkSeenBy(User user)
+        {
+            if (!SeenBy.Contains(user))
+                SeenBy.Add(user);
+        }
+    }
+}
