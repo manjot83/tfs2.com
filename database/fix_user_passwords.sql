c7654d2f7af9415f102b9c054a8480ff8f1aef96
Joseph Daigle <joseph@cridion.com>
2014-12-21 20:34:34 -0500

    minor script tweak

--- database/fix_user_passwords.sql
+++ database/fix_user_passwords.sql
@@ -1,5 +1,8 @@
 IF NOT EXISTS (SELECT * FROM sys.columns WHERE name = 'PasswordHash' and object_id = OBJECT_ID('dbo.Users'))
 BEGIN
 ALTER TABLE dbo.Users ADD PasswordHash nvarchar(1000) NULL;
-UPDATE dbo.Users SET PasswordHash = 'vz95XUvetbBwqHjaZLrXmmwqTB8wOXaNjf1u6F6EX1U=' WHERE PasswordHash IS NULL;
 END
+GO
+
+UPDATE dbo.Users SET PasswordHash = 'vz95XUvetbBwqHjaZLrXmmwqTB8wOXaNjf1u6F6EX1U=' WHERE PasswordHash IS NULL;
+GO
